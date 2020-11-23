221387b5d6c847d5bcdd03a5cc274b4c77c32dbf
antonio <antonio@myemail.com>
2020-11-23 22:54:37 +0000

    Add deconz instalation

--- install.sh
+++ install.sh
@@ -3,3 +3,11 @@ docker-compose -f ~/homehub/docker/hassio/docker-compose.yml up -d
 mkdir ../config/unifi && cd ../config/unifi
 curl -O https://raw.githubusercontent.com/ryansch/docker-unifi-rpi/master/docker-compose.yml
 docker-compose up -d
+
+# deConz
+
+# Set user USB access rights
+sudo gpasswd -a pi dialout
+
+mkdir -p ~/config/deCONZ
+docker-compose -f ~/homehub/docker/deConz/docker-compose.yml up -d
