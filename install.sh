aee88f6cede624cd15e20d02c7a632c72568c834
antonio <antonio@myemail.com>
2021-01-24 17:25:31 +0000

    add mosquito. Remover deconz

--- install.sh
+++ install.sh
@@ -9,5 +9,13 @@ docker-compose up -d
 # Set user USB access rights
 sudo gpasswd -a pi dialout
 
-mkdir -p ~/config/deCONZ
-docker-compose -f ~/homehub/docker/deConz/docker-compose.yml up -d
+# mkdir -p ~/config/deCONZ
+# docker-compose -f ~/homehub/docker/deConz/docker-compose.yml up -d
+
+mkdir -p ~/config/mosquitto
+mkdir -p ~/config/mosquitto/data
+mkdir -p ~/config/mosquitto/log
+echo "persistence true" > ~/config/mosquitto/mosquitto.conf
+echo "persistence_location /mosquitto/data/" >> ~/config/mosquitto/mosquitto.conf
+echo "log_dest file /mosquitto/log/mosquitto.log" >> ~/config/mosquitto/mosquitto.conf
+docker-compose -f ~/homehub/docker/mosquitto/docker-compose.yml up -d
