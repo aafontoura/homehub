a68375bb96dfa915d6eed12d1d583604fedd2a15
antonio <antonio@myemail.com>
2021-03-11 20:57:24 +0000

    media server docker

---
+++ install-media_server.sh
@@ -0,0 +1,3 @@
+# mkdir -p ~/config/zigbee2mqtt
+docker-compose -f ~/homehub/docker/media_server/docker-compose.yml up -d
+
--- install-system.sh
+++ install-system.sh
@@ -1,6 +1,10 @@
 # install docker
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker pi
+
+# wget http://ftp.us.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.4.4-1~bpo10+1_armhf.deb
+# sudo dpkg -i libseccomp2_2.4.4-1~bpo10+1_armhf.deb
+
 # install portainer image
 docker pull portainer/portainer:linux-arm
 sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm
