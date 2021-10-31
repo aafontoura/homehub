08635ace00c7a0d632c68a3a338576842b69af53
aafontoura <antonio@antonio>
2021-10-31 15:28:03 +0100

    plex dependencies and omv config

--- install-media_server.sh
+++ install-media_server.sh
@@ -4,6 +4,9 @@ then
     mkdir ${MEDIA_FOLDER}/series
     mkdir ${MEDIA_FOLDER}/movies
 
+    # install dependencies for plex
+    sudo apt install ocl-icd-libopencl1 beignet-opencl-icd -y
+
     docker-compose -f ~/homehub/docker/media_server/docker-compose.yml up -d
 else
     echo "inform media folder"
