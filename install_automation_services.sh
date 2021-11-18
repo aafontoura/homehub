fd68ba43b2b33a3bb33205bb008504287b400813
aafontoura <antonio@antonio>
2021-11-18 22:25:05 +0100

    installation script for automation

---
+++ install_automation_services.sh
@@ -0,0 +1,8 @@
+cp automation/*.service /etc/systemd/system
+
+systemctl enable automation-ventilation.service
+systemctl start automation-ventilation.service
+systemctl enable logger-ventilation.service
+systemctl start logger-ventilation.service
+systemctl enable storage-light.service
+systemctl start storage-light.service
