9a26a81b513acb25fdda22e326a8f2fa78c69633
aafontoura <antonio@antonio>
2021-11-18 22:18:41 +0100

    portainer for amd64

--- update_portainer.sh
+++ update_portainer.sh
@@ -1,5 +1,5 @@
-docker pull portainer/portainer:linux-arm
-sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm
+docker pull portainer/portainer-ce:linux-amd64
+sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-amd64
 
 # docker-compose -f ~/homehub/docker/zigbee2mqtt/docker-compose.yml pull
 # docker-compose -f ~/homehub/docker/zigbee2mqtt/docker-compose.yml up -d
