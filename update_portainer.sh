c78eabdf8b33b9359ff7f0cdc925112c30db306c
antonio <antonio@myemail.com>
2021-12-31 14:42:50 +0000

    portainer install for arm and x84

--- update_portainer.sh
+++ update_portainer.sh
@@ -1,5 +1,16 @@
-docker pull portainer/portainer-ce:linux-amd64
-sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-amd64
-
-# docker-compose -f ~/homehub/docker/zigbee2mqtt/docker-compose.yml pull
-# docker-compose -f ~/homehub/docker/zigbee2mqtt/docker-compose.yml up -d
+if [ -z $1 ]
+then
+    echo "Provide architecture (arm,x64)"
+elif [ $1 == "arm" ]
+    then
+        docker pull portainer/portainer-ce:linux-arm-2.0.0-alpine
+        sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-arm-2.0.0-alpine
+        
+    elif [ $1 == "x64" ]
+    then
+        docker pull portainer/portainer-ce:linux-amd64
+        sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-amd64
+    else
+        echo "Architecture not recognized: $1"
+    fi
+fi
