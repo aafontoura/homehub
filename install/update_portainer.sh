35c6cecea0c512a10419245998aa6d6909409370
Antonio <antonio.fontoura@gmail.com>
2024-08-25 13:01:04 +0000

    include name on portainer docker run script

--- install/update_portainer.sh
+++ install/update_portainer.sh
@@ -4,12 +4,12 @@ then
 elif [ $1 == "arm" ]
     then
         docker pull portainer/portainer-ce:linux-arm-2.0.0-alpine
-        sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-arm-2.0.0-alpine
+        sudo docker run --restart always -d -p 9000:9000 portainer  -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-arm-2.0.0-alpine
         
     elif [ $1 == "x64" ]
     then
         docker pull portainer/portainer-ce:linux-amd64
-        sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-amd64
+        sudo docker run --restart always -d -p 9000:9000 portainer -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-amd64
     else
         echo "Architecture not recognized: $1"
     fi
