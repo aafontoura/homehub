422559b4650b8d8b393161b4238e5a6c28b66117
Antonio <antonio.fontoura@gmail.com>
2024-08-25 13:00:33 +0000

    update appdaemon installation script

--- install/install_appdaemon.sh
+++ install/install_appdaemon.sh
@@ -1,4 +1,11 @@
+
+docker exec -it appdaemon-appdaemon-1 kill 1
+docker stop appdaemon-appdaemon-1
+
+rm -r ~/homehub/automation/src/appdaemon/__pycache__
 cp -r ~/homehub/automation/src/appdaemon/* ~/appdata/appdaemon/apps
 cp -r ~/homehub/automation/src/energycalculation.py ~/appdata/appdaemon/apps/src
+
+./start_image.sh appdaemon
 
 
